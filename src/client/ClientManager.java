92183451e7c2d05cc10c403ae04267e7524f7323
Mattia Sorgato <mattia.sorgato@gmail.com>
2014-09-10 13:16:10 +0200

    Ora i main dovrebbero essere a posto

--- src/client/ClientManager.java
+++ src/client/ClientManager.java
@@ -1,52 +1,63 @@
 package client;
 
-import java.net.MalformedURLException;
 import java.rmi.Naming;
-import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
-import server.Server;	//ricordati di levarlo
 
 public class ClientManager {
+	
+	public ClientManager(String[] args) throws Exception {
+		String clientName = args[0], serverName = args[1];
+		int maxDown;
+		try {
+			maxDown = Integer.parseInt(args[2]);
+		} catch(NumberFormatException e) {
+			throw new Exception();
+		}
+		ArrayList<Resource> resources = new ArrayList<Resource>();
+		
+		for(int i = 3; i < args.length; i += 2) {
+			String name = args[i];
+			int parts;
+			try {
+				parts = Integer.parseInt(args[i + 1]);
+			} catch(NumberFormatException e) {
+				throw new Exception();
+			}
+			resources.add(createResource(name, parts));
+			
+		}
+		try {
+			Client client = new ConcreteClient(clientName, maxDown, resources, serverName);
+		} catch(RemoteException ex) {
+			ex.printStackTrace();
+			throw new Exception();
+		}
+	}
+	
+	private Resource createResource(String name, int parts) {
+		ResourceFragment[] frags = new ResourceFragment[parts];
+		for(int i = 0; i < parts; i++)
+			frags[i] = new ResourceFragment(name, parts, i + 1);
+		return new Resource(name, parts, frags);
+		
+	}
 
 	public static void main(String[] args) {
-		ResourceFragment[] frags = new ResourceFragment[5], frags2 = new ResourceFragment[7], frags5 = new ResourceFragment[5];
-		for(int i = 0; i < 5; i++) {
-			frags[i] = new ResourceFragment("uno", 5, i + 1);
-			frags5[i] = new ResourceFragment("tre", 5, i + 1);
-		}
-		for(int i = 0; i < 7; i++) {
-			frags2[i] = new ResourceFragment("due", 7, i + 1);
-		}
-		ResourceFragment[] frags3 = new ResourceFragment[5], frags4 = new ResourceFragment[7];
-		for(int i = 0; i < 5; i++) {
-			frags3[i] = new ResourceFragment("tre", 5, i + 1);
-		}
-		for(int i = 0; i < 7; i++) {
-			frags4[i] = new ResourceFragment("quattro", 7, i + 1);
-		}
-		Resource resource = new Resource("uno", 5, frags), resource2 = new Resource("due", 7, frags2), resource3 = new Resource("tre", 5, frags3),
-				resource4 = new Resource("quattro", 7, frags4), resource5 = new Resource("tre", 5, frags5);	
-		ArrayList<Resource> resources = new ArrayList<Resource>(), resources2 = new ArrayList<Resource>(), resources3 = new ArrayList<Resource>();
-		resources.add(resource);
-		resources.add(resource2);
-		resources2.add(resource3);
-		resources2.add(resource4);
-		resources3.add(resource5);
-		Client client1 = null, client2 = null, client3 = null;
+		if(args.length < 3 || (args.length % 2) == 0 || args[0] == "--help") { //se la lista degli argomenti e' pari, ci sono probabilmente risorse incomplete
+			System.out.println("Il comando deve avere come argomenti: nome del client (Stringa) - nome del server (Stringa) - numero di download "
+					+ "concorrenti massimi (Intero) - [opzionale] risorse (nome risorsa (Stringa) - parti risorsa (Intero)");
+			try {
+				Thread.sleep(2500);
+			} catch (InterruptedException e) {}
+			System.exit(0);
+		}	
 		try {
-			client1 = new ConcreteClient("FooClient1", 2, resources, "Razorback");
-			client2 = new ConcreteClient("FooClient2", 2, resources2, "Razorback");
-			client3 = new ConcreteClient("FooClient3", 2, resources3, "Razorback");
-		} catch(RemoteException e) {
+			ClientManager manager = new ClientManager(args);
+		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		try {
-			client1.download("tre", 5);
-		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("La creazione del Client ha lanciato un'eccezione. Probabilmente le risorse sono in formato sbagliato.");
 		}
 	}
 
--- src/server/ServerManager.java
+++ src/server/ServerManager.java
@@ -8,8 +8,7 @@ public class ServerManager {
 		try {
 			Server server = new ConcreteServer(serverName);
 		} catch (RemoteException e) {
-			System.out.println("Sono occorsi problemi nella creazione del Server.\n"
-					+ "Verificare che il comando rmiregistry sia stato avviato correttamente.");
+			System.out.println("Sono occorsi problemi nella creazione del Server. Verificare che il comando rmiregistry sia stato avviato correttamente.");
 			e.printStackTrace();
 			try {
 				Thread.sleep(2000);
@@ -26,24 +25,14 @@ public class ServerManager {
 	
 	public static void main(String[] args) {
 		if(args.length == 0 || args[0] == "--help") {
-			System.out.println("\nIl comando di avvio necessita di un argomento String valido\n"
-					+ "da assegnare come nome del Server.");
+			System.out.println("\nIl comando di avvio necessita di un argomento String valido da assegnare come nome del Server.");
+			try {
+				Thread.sleep(2000);
+			} catch(InterruptedException ex) {}
 			System.exit(0);				
 		}	
 		
 		ServerManager manager = new ServerManager(args[0]);
-		/*
-		//System.out.println("Current working directory: " + System.getProperty("user.dir"));
-		Server razor;
-		try {
-			razor = new ConcreteServer("Razorback");
-		} catch(RemoteException e) {
-			e.printStackTrace();
-			System.out.println("Probabilmente il registro RMI non è stato avviato");
-		} catch (AlreadyBoundedException e) {
-			e.printStackTrace();
-		}
-		System.out.println("Finito il main del Server");*/
 	}
 
 }
